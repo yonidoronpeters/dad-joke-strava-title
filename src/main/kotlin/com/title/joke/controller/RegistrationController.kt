34e04056fd38d142300de37561d475a7a37f9b9f
Yoni Doron-Peters <yonidoronpeters@gmail.com>
2021-08-08 00:50:38 -0600

    Add some logging

--- src/main/kotlin/com/title/joke/controller/RegistrationController.kt
+++ src/main/kotlin/com/title/joke/controller/RegistrationController.kt
@@ -1,5 +1,6 @@
 package com.title.joke.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,9 +13,11 @@ class RegistrationController(
     @Value("\${strava.auth.authorize-url}")
     val redirectUrl: String
 ) {
+    private val logger = LoggerFactory.getLogger(RegistrationController::class.java)
 
     @GetMapping
     fun redirectToStrava(response: HttpServletResponse) {
+        logger.info("Redirect url is: $redirectUrl")
         return response.sendRedirect(redirectUrl)
     }
 }
