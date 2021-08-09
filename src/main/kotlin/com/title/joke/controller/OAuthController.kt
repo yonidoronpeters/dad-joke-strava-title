fc2338ca3ca882d1c543bdca53bb469d9c78013a
Yoni Doron-Peters <yonidoronpeters@gmail.com>
2021-08-08 23:18:53 -0600

    Finish html pages

--- src/main/kotlin/com/title/joke/controller/OAuthController.kt
+++ src/main/kotlin/com/title/joke/controller/OAuthController.kt
@@ -3,18 +3,17 @@ package com.title.joke.controller
 import com.title.joke.service.OAuthTokenService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 
 
-@RestController
+@Controller
 @RequestMapping("oauth")
 class OAuthController(
     private val service: OAuthTokenService
 ) {
-
     private val logger: Logger = LoggerFactory.getLogger(OAuthController::class.java)
 
     @GetMapping("/exchange_token")
@@ -24,9 +23,9 @@ class OAuthController(
     ): String {
         logger.info("user permitted scopes: $scopes")
         if (!scopes.contains("activity:write")) {
-            return "App doesn't work without write permissions. Please try again and check the box for 'Upload your activities from Dad joke title generator to Strava'."
+            return "error"
         }
         service.authorizeApp(code)
-        return "authorization successful"
+        return "success"
     }
 }
