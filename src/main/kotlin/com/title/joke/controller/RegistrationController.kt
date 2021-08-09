d8b10852405c47421ea0f824e47aaedb46ce2cd9
Yoni Doron-Peters <yonidoronpeters@gmail.com>
2021-08-08 22:36:56 -0600

    Update homepage

--- src/main/kotlin/com/title/joke/controller/RegistrationController.kt
+++ src/main/kotlin/com/title/joke/controller/RegistrationController.kt
@@ -2,22 +2,26 @@ package com.title.joke.controller
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
 
-@RestController
-@RequestMapping("register")
+@Controller
 class RegistrationController(
     @Value("\${strava.auth.authorize-url}")
     val redirectUrl: String
 ) {
     private val logger = LoggerFactory.getLogger(RegistrationController::class.java)
 
-    @GetMapping
+    @GetMapping("register")
     fun redirectToStrava(response: HttpServletResponse) {
         logger.info("Redirect url is: $redirectUrl")
         return response.sendRedirect(redirectUrl)
+    }
+
+    @RequestMapping("/")
+    internal fun index(): String {
+        return "index"
     }
 }
