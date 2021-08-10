96560e00f15ba528ea9d3da9fc18e14439dece9a
Yoni Doron-Peters <yonidoronpeters@gmail.com>
2021-08-09 23:16:56 -0600

    Verify subscription id for webhook events

--- src/main/kotlin/com/title/joke/controller/EventsController.kt
+++ src/main/kotlin/com/title/joke/controller/EventsController.kt
@@ -3,18 +3,28 @@ package com.title.joke.controller
 import com.title.joke.service.ActivityService
 import com.title.joke.dto.EventDataDto
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("subscribe")
-class EventsController(private val activityService: ActivityService) {
+class EventsController(
+    @Value("\${strava.subscription-id}")
+    val subscriptionId: String,
+    private val activityService: ActivityService
+) {
     private val logger = LoggerFactory.getLogger(EventsController::class.java)
 
     @PostMapping("callback", consumes = ["application/json"])
     fun callback(@RequestBody eventData: EventDataDto): ResponseEntity<String> {
         logger.info("Received event: $eventData")
-        activityService.updateActivity(eventData)
-        return ResponseEntity.ok().build()
+        if (subscriptionId == eventData.subscription_id) {
+            activityService.updateActivity(eventData)
+            return ResponseEntity.ok().build()
+        }
+        logger.warn("Unauthorized event received: $eventData")
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
     }
 }
